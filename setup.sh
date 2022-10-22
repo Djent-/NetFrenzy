79d35be5cfc2735fc6aee1251aa5e966a5e97784
Djent- <patrick.hurd.1995@gmail.com>
2022-10-22 14:55:31 -0400

    Update neo4j plugin directory location

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 # Install our own requirements
 python3 -m pip install -r requirements.txt
 
+sudo mkdir -p /usr/share/neo4j/plugins
 # Download the graph data library from neo4j
 # Install the graph data library into the plugins folder
-wget https://github.com/neo4j/graph-data-science/releases/download/2.1.6/neo4j-graph-data-science-2.1.6.jar -O /var/lib/neo4j/plugins/neo4j-graph-data-science-2.1.6.jar
-sudo chown neo4j:adm /var/lib/neo4j/plugins/neo4j-graph-data-science-1.8.3.jar
+wget https://github.com/neo4j/graph-data-science/releases/download/2.1.6/neo4j-graph-data-science-2.1.6.jar -O /usr/share/neo4j/plugins/neo4j-graph-data-science-2.1.6.jar
 
 # Change config
 echo "dbms.security.procedures.unrestricted=gds.*" | sudo tee -a /etc/neo4j/neo4j.conf
